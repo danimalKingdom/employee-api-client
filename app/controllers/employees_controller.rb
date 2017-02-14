083f3014d491ba9ca27e4b87b180e7880d7a43ec
dani <dani.zaghian@gmail.com>
2017-02-13 21:41:34 -0800

    added edit, update, and delete functions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -13,4 +13,22 @@ class EmployeesController < ApplicationController
     @employee = Unirest.post("http://localhost:3000/api/v2/employees", :parameters => {first_name: params[:first_name], last_name: params[:last_name], email: params[:email]}).body
     redirect_to employee_path(@employee['id'])
   end
+  def edit
+    id = params[:id]
+    @employee = Unirest.get("http://localhost:3000/api/v2/employees/#{id}").body
+  end
+  def update
+    id = params[:id]
+    @employee = Unirest.patch("http://localhost:3000/api/v2/employees/#{id}", :parameters => {first_name: params[:first_name], last_name: params[:last_name], email: params[:email]}).body
+    redirect_to employee_path(@employee['id'])
+  end
+  def destroy
+    id = params[:id]
+    @employee = Unirest.delete("http://localhost:3000/api/v2/employees/#{id}").body
+    redirect_to employees_path
+  end
+
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get '/employees' => 'employees#index'
   get '/employees/new' => 'employees#new'
   post '/employees' => 'employees#create'
+  get '/employees/:id/edit' => 'employees#edit'
+  patch '/employees/:id' => 'employees#update'
   get '/employees/:id' => 'employees#show', as: 'employee'
+  delete '/employees/:id' => 'employees#destroy'
 end
