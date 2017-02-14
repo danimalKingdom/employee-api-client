ada556197a7b6180795587786f676b00d9cc8191
dani <dani.zaghian@gmail.com>
2017-02-13 20:06:14 -0800

    added index route controller and view consuming api data

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,7 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Unirest.get("http://localhost:3000/api/v2/employees").body
+  end
   def show
     id = params[:id]
     @employee = Unirest.get("http://localhost:3000/api/v2/employees/#{id}").body
