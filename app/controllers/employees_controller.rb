8f38759ea49113bb5562effd86b20af4d81bb51f
dani <dani.zaghian@gmail.com>
2017-02-13 20:43:17 -0800

    added create method that posts to employee api

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,4 +6,11 @@ class EmployeesController < ApplicationController
     id = params[:id]
     @employee = Unirest.get("http://localhost:3000/api/v2/employees/#{id}").body
   end
+  def new
+    render :new
+  end
+  def create
+    @employee = Unirest.post("http://localhost:3000/api/v2/employees", :parameters => {first_name: params[:first_name], last_name: params[:last_name], email: params[:email]}).body
+    redirect_to employee_path(@employee['id'])
+  end
 end
